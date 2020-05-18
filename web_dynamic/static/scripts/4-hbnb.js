7960e524acb6d24c6e3e24ade2043b769609852f
Thorlak2202 <darkdrakkar@gmail.com>
2020-05-18 11:35:08 -0500

    refator of scrips 3 and 4 JavaScript

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -73,7 +73,19 @@ function filterAmenities () {
         success: function (data) {
         for (let i = 0; i < data.length; i++) {
             let place = data[i];
-            $('.places ').append('<article><div class="title_box"><h2>' + place.name + '</h2><div class="price_by_night">$' + place.price_by_night + '</div></div><div class="information"><div class="max_guest">' + place.max_guest + '</div><div class="number_rooms">' + place.number_rooms +'</div><div class="number_bathrooms">' + place.number_bathrooms + '</div></div><div class="description">' +place.description + '</div></article>')
+            $('.places ').append('<article><div class="title_box"><h2>'
+            + place.name +
+            '</h2><div class="price_by_night">$'
+            + place.price_by_night +
+            '</div></div><div class="information"><div class="max_guest">'
+            + place.max_guest +
+            '</div><div class="number_rooms">'
+            + place.number_rooms +
+            '</div><div class="number_bathrooms">'
+            + place.number_bathrooms +
+            '</div></div><div class="description">'
+            +place.description +
+            '</div></article>')
         }
         },
         error: function (data) {
